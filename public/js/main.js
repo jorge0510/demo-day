b7a41964d9f8acd01f161778c44f88a93808e160
Jorge Garcia <jagz0510@live.com>
2025-05-13 14:59:28 -0400

    modified no review instead of 5 stars as default

--- public/js/main.js
+++ public/js/main.js
@@ -504,7 +504,7 @@ const displayRating = (reviewData) =>
     ? addPointZero(
         reviewData.reduce((a, c) => a + c.rating, 0) / reviewData.length
       )
-    : "5.0";
+    : "";
 
 const renderBusinesses = (businessList) => {
   numberOfBusinessesFound.innerText = businessList.length || 0;
@@ -711,7 +711,7 @@ function renderFeaturedBusinesses(featuredList = []) {
               <svg class="h-3 w-3 fill-yellow-500 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
               </svg>
-              <span>${ displayRating(biz.reviewData) || 'N/A'}</span>
+              <span>${ displayRating(biz.reviewData) || 'No Reviews'}</span>
             </div>
           </div>
         </div>
