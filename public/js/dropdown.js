3c971df8f250bdb0139653da9de865d5bb4484bc
Jorge Garcia <jagz0510@live.com>
2025-05-15 01:15:59 -0400

    add responsivenes on mobile

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,14 +1,19 @@
-const dropdownBtn = document.getElementById('profileDropdownBtn');
-const dropdownMenu = document.getElementById('profileDropdown');
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('profileDropdownBtn');
+    const dropdown = document.getElementById('profileDropdown');
+    const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    const mobileMenu = document.getElementById('mobileMenu');
 
-if(dropdownBtn){
-    dropdownBtn.addEventListener('click', function (e) {
-    e.stopPropagation(); 
-    dropdownMenu.classList.toggle('hidden');
+    mobileMenuBtn?.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+    btn?.addEventListener('click', () => {
+      dropdown.classList.toggle('hidden');
     });
 
-
-    window.addEventListener('click', function () {
-    dropdownMenu.classList.add('hidden');
+    document.addEventListener('click', (e) => {
+      if (!btn.contains(e.target) && !dropdown.contains(e.target)) {
+        dropdown.classList.add('hidden');
+      }
     });
-}
+  });
